d40166201690be63e8c89792a08f199c44215761
Lucas Sant <lucasfdesantana095@gmail.com>
2025-11-28 01:13:46 -0300

    feat: página principal do agricultor

---
+++ agrosense/src/pages/Agricultor/FirstPage/FirstPageAgricultor.jsx
@@ -0,0 +1,11 @@
+function FirstPageAgricultor(){
+    return (
+        <>
+            <h1>
+                Olá Agricultor
+            </h1>
+        </>
+    )
+}
+
+export default FirstPageAgricultor;
