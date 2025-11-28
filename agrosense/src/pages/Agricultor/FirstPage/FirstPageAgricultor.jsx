0a100e209a907c3b4c6811f8fc99d40c4deeba31
Lucas Sant <lucasfdesantana095@gmail.com>
2025-11-28 15:32:17 -0300

    feat: iniciando estruturacao da pagina

--- agrosense/src/pages/Agricultor/FirstPage/FirstPageAgricultor.jsx
+++ agrosense/src/pages/Agricultor/FirstPage/FirstPageAgricultor.jsx
@@ -1,9 +1,41 @@
+import AgrosenseLogo from '../../assets/image/agrosense-logo.png';
+import { Navigate, useNavigate } from 'react-router-dom';
+
 function FirstPageAgricultor(){
+
+    const navigate = useNavigate()
+    function Setting(){
+        
+    }
+
     return (
         <>
-            <h1>
-                Olá Agricultor
-            </h1>
+           <header>
+                <div>
+                    <img src={AgrosenseLogo} />
+                    <h4>Agrosense</h4>
+
+                    config
+                </div>
+           </header>
+           <main>
+                <section className='user-request'>
+                    <div>
+                        {/* <img src={} /> */}
+                        <h4>Olá, José Silva</h4>
+                        <p>***.***.*89-00</p>
+                    </div>
+                    <div>
+
+                    </div>
+                </section>
+                <section className='seeds-grains-request'>
+
+                </section>
+                <section className='message-end'>
+                    
+                </section>
+           </main>
         </>
     )
 }
