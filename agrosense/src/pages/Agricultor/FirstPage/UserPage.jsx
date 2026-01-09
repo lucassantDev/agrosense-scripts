88554a0235d794efcbd985933c47a6370c752889
Lucas Sant <lucasfdesantana095@gmail.com>
2026-01-09 10:39:54 -0300

    feat: adicionando sementes e graos na section

--- agrosense/src/pages/Agricultor/FirstPage/UserPage.jsx
+++ agrosense/src/pages/Agricultor/FirstPage/UserPage.jsx
@@ -8,6 +8,13 @@ import Agricultor from '../../../assets/image/agricultor.jpg'
 import Logo from '../../../assets/image/agrosense-logo.png'
 import Config from '../../../assets/icon/settings.svg';
 
+import AlfaceImg from '../../../assets/image/alface.jpg'
+import MilhoImg from '../../../assets/image/milho.jpg'
+import CenouraImg from '../../../assets/image/cenoura.jpg'
+import FeijaoImg from '../../../assets/image/feijao.jpg'
+import TomateImg from '../../../assets/image/tomate.jpg'
+import PimentaoImg from '../../../assets/image/pimentao.jpg'
+
 
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
@@ -26,6 +33,7 @@ function UserPage(){
     function InSettings(){
         navigate('/Configuracoes/Settings')
     }
+
     return (
         <>
             <header className='header-agrosense'>
@@ -46,9 +54,6 @@ function UserPage(){
 
             <main>
                 <section className='name-user'>
-                    {/* <div className='image-agricultor'>
-                    </div> */}
-
                     <img src={JoseSilva} alt="" className='image-agricultor' />
                     <div className='name-agricultor'>
                         <h2>Olá, José Silva</h2>
@@ -68,7 +73,56 @@ function UserPage(){
                 </section>
 
                 <section className='requests-user'>
-
+                    <div className='requests-user-message'>
+                        <h2>
+                            Visualize as sementes e grãos e solicite a desejada!
+                        </h2>
+                    </div>
+                    <div className='seeds-grains'>
+                        {/* futuramente aqui poderia ser um map */}
+                        <div className="image-seed-grain">
+                            <img src={MilhoImg} alt="imagem alface"/>
+                            <h2>
+                                Milho
+                            </h2>
+                            <button>solicitar</button>
+                        </div>
+                        <div className="image-seed-grain">
+                            <img src={AlfaceImg} alt="imagem alface"/>
+                            <h2>
+                                Alface
+                            </h2>
+                            <button>solicitar</button>
+                        </div>
+                        <div className="image-seed-grain">
+                            <img src={CenouraImg} alt="imagem alface"/>
+                            <h2>
+                                Cenoura
+                            </h2>
+                            <button>solicitar</button>
+                        </div>
+                        <div className="image-seed-grain">
+                            <img src={FeijaoImg} alt="imagem alface"/>
+                            <h2>
+                                Feijão
+                            </h2>
+                            <button>solicitar</button>
+                        </div>
+                        <div className="image-seed-grain">
+                            <img src={TomateImg} alt="imagem alface"/>
+                            <h2>
+                                Tomate
+                            </h2>
+                            <button>solicitar</button>
+                        </div>
+                        <div className="image-seed-grain">
+                            <img src={PimentaoImg} alt="imagem alface"/>
+                            <h2>
+                                Pimentão
+                            </h2>
+                            <button>solicitar</button>
+                        </div>
+                    </div>
                 </section>
 
                 <section className='message-page'>
