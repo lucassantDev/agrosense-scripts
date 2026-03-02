9be0dfc80ac082dbda3cee5836192e16b4d26761
Lucas Sant <lucasfdesantana095@gmail.com>
2026-03-01 21:48:46 -0300

    feat: map de lotes e acompanhamento

--- agrosense/src/pages/Administrativo/FirstPageAdm/AdmPage.jsx
+++ agrosense/src/pages/Administrativo/FirstPageAdm/AdmPage.jsx
@@ -2,9 +2,11 @@ import Logo from '../../../assets/image/agrosense-logo.png'
 import Config from '../../../assets/icon/settings.svg';
 import Footer from '../../../assets/components/Footer.jsx';
 
-import Truck from '../../../assets/image/caminhao-icon.png'
+import Truck from '../../../assets/image/caminhao-icon.png';
+import Box from '../../../assets/image/box-agrosense.png';
+import './AdmPage.css';
 
-import './AdmPage.css'
+import { useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
@@ -15,6 +17,68 @@ function FirstPageAdm(){
     function InSettings(){
         navigate('/Configuracoes/Settings')
     }
+
+    const [ships] = useState([
+        {
+            id: 1,
+            number: '201023',
+            to: 'Vitória',
+            message: 'Saiu do centro de distribuição',
+            hour: '07:21 AM',
+            image: Truck
+        },
+        {
+            id: 2,
+            number: '201023',
+            to: 'Garanhuns',
+            message: 'Em preparação',
+            hour: '--:--',
+            image: Truck
+        },
+        {
+            id: 3,
+            number: '201023',
+            to: 'Garanhuns',
+            message: 'Em preparação',
+            hour: '--:--',
+            image: Truck
+        },
+        {
+            id: 4,
+            number: '201023',
+            to: 'Águas Belas',
+            message: 'Aguardando confirmação',
+            hour: '--:--',
+            image: Truck
+        },
+    ])
+
+    const [allotment] = useState([
+        {
+            id: 1,
+            grain: 'Fejão',
+            destination: 'Vitória',
+            image: Box
+        },
+        {
+            id: 2,
+            grain: 'Arroz',
+            destination: 'Garanhuns',
+            image: Box
+        },
+        {
+            id: 3,
+            grain: 'Fejão',
+            destination: 'Garanhuns',
+            image: Box
+        },
+        {
+            id: 4,
+            grain: 'Pimentão Verde',
+            destination: 'Águas Belas',
+            image: Box
+        }
+    ])
 
     return (
         <>
@@ -39,18 +103,20 @@ function FirstPageAdm(){
                     <div className="adm-actions"></div>
                 </section>
                 <section className='section-ship xAndY'>
-                    <div className="auto">
-                        <img src={Truck} alt="imagem 3d de um caminhão" />
-                    </div>
-                    <div className="auto">
-                        <img src={Truck} alt="imagem 3d de um caminhão" />
-                    </div>
-                    <div className="auto">
-                        <img src={Truck} alt="imagem 3d de um caminhão" />
-                    </div>
-                    <div className="auto">
-                        <img src={Truck} alt="imagem 3d de um caminhão" />
-                    </div>
+                    {
+                        ships.map(ship=>(
+                            <div className='div-ship'>
+                                <img src={ship.image} className='truck' alt="imagem 3d caminhão" />
+                                <div className='ship-info'>
+                                    <p className='number-map secondary-font'>#{ship.number} - {ship.to}</p>
+                                    <p className='message-map secondary-font'>{ship.message}</p>
+                                    <p className='hour-map secondary-font'>
+                                        {ship.hour}
+                                    </p>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </section>
             </main>
 
